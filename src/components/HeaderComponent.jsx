1fae975161aab63accf948407130e35bf449e5a0
CodeAadarsh <aadarsh24lv@gmail.com>
2024-09-08 20:50:36 +0530

    Navigation and Tab bar done

--- src/components/HeaderComponent.jsx
+++ src/components/HeaderComponent.jsx
@@ -2,11 +2,12 @@ import { View, Text } from 'react-native'
 import React from 'react'
 import { TouchableOpacity } from 'react-native-gesture-handler'
 import ChevronLeft from '../../assets/SVG_Jsx/ChevronLeft'
-
+import { useNavigation } from '@react-navigation/native'
 const HeaderComponent = ( { headerDetails} ) => {
+  const navigation = useNavigation();
   return (
     <View>
-       <TouchableOpacity className='px-8 py-4 flex items-center flex-row pt-16'>
+       <TouchableOpacity onPress={()=>navigation.goBack()} className='px-8 py-4 flex items-center flex-row pt-16'>
                 <View className=''>
                     <ChevronLeft />
                 </View>
--- src/routes/TabBar/MainTabBar.jsx
+++ src/routes/TabBar/MainTabBar.jsx
@@ -1,54 +1,60 @@
+import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons'; // or use another icon library
+import {HomeIcon,MagnifyingGlassIcon,HeartIcon, ShoppingBagIcon,UserCircleIcon} from 'react-native-heroicons/outline'
+import {HomeIcon as HomeMini, MagnifyingGlassIcon as MagnifyingGlassIconMini, HeartIcon as HeartIconMini, ShoppingBagIcon as ShoppingBagIconMini, UserCircleIcon as UserCircleIconMini} from 'react-native-heroicons/mini'
 import Homepage from '../../screens/Homepage';
 import SearchPage from '../../screens/SearchPage';
 import WishList from '../../screens/WishList';
 import MyCart from '../../screens/MyCart';
-import AccountPage from '../../screens/AccountPage';
 import AccountpageRoutes from '../Accountpage/AccountpageRoutes';
-import LoginPage from '../../screens/LoginPage';
-import SignUp from '../../screens/SignUp';
-import OtpPage from '../../screens/OtpPage';
-import MobileLogin from '../../screens/MobileLogin';
-import ForgotPassword from '../../screens/ForgotPassword';
-import MyRewards from '../../screens/MyRewards';
-import MyOrders from '../../screens/MyOrders';
-import MyOrders1 from '../../screens/MyOrders1';
-import EditProfile from '../../screens/EditProfile';
-import ProductCard from '../../components/ProductCard';
-import Productspage from '../../screens/Productspage';
 
 const Tab = createBottomTabNavigator();
 
 function MainTabBar() {
   return (
     <Tab.Navigator
-    screenOptions={({ route }) => ({
-      headerShown:false,
-      // tabBarIcon: ({ focused, color, size }) => {
-      //   let iconName;
+      screenOptions={({ route }) => ({
+        headerShown: false,
+        tabBarStyle: {
+          backgroundColor: '#A50102', // Your primary color
+          borderTopWidth: 0, // To remove the default border
+          height: 75,
+          alignItems:'center',
+          alignContent:'center'  
+        },
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
 
-      //   if (route.name === 'Home') {
-      //     iconName = focused
-      //       ? 'ios-information-circle'
-      //       : 'ios-information-circle-outline';
-      //   } else if (route.name === 'Settings') {
-      //     iconName = focused ? 'ios-list' : 'ios-list-outline';
-      //   }
+          // Choose icons based on the route name
+          if (route.name === 'Home') {
+            iconName = focused ? <HomeMini size={25} color={'#fff'}/>:<HomeIcon size={25} color={'#fff'}/>  ;
+          } else if (route.name === 'Search') {
+            iconName = focused ? <MagnifyingGlassIconMini size={25} color={'#fff'}/> : <MagnifyingGlassIcon size={25} color={'#fff'}/>;
+          } else if (route.name === 'WishList') {
+            iconName = focused ? <HeartIconMini size={25} color={'#fff'}/> : <HeartIcon size={25} color={'#fff'}/>;
+          } else if (route.name === 'MyCart') {
+            iconName = focused ? <ShoppingBagIconMini size={25} color={'#fff'}/> : <ShoppingBagIcon size={25} color={'#fff'}/>;
+          } else if (route.name === 'accountspage') {
+            iconName = focused ? <UserCircleIconMini size={25} color={'#fff'}/> : <UserCircleIcon size={25} color={'#fff'}/>;
+          }
 
-      //   // You can return any component that you like here!
-      //   return <Ionicons name={iconName} size={size} color={color} />;
-      // },
-      // tabBarActiveTintColor: 'tomato',
-      // tabBarInactiveTintColor: 'gray',
-    })}
-  >
-    <Tab.Screen name="Home" component={Homepage} />
-    <Tab.Screen name="Search" component={SearchPage} />
-    <Tab.Screen name="WishList" component={WishList} />
-    <Tab.Screen name="MyCart" component={MyCart} />
-    <Tab.Screen name="accountspage" component={AccountpageRoutes} />
-  </Tab.Navigator>
+          // Render icons from Ionicons
+          // return <Ionicons name={iconName} size={20} color={color} />;
+          return iconName
+        },
+        tabBarActiveTintColor: '#fff', // White color for active icons
+        tabBarInactiveTintColor: '#fff', // White color for inactive icons
+        tabBarShowLabel: false, // Hide labels to match the provided image
+      })}
+    >
+      <Tab.Screen name="Home" component={Homepage} />
+      <Tab.Screen name="Search" component={SearchPage} />
+      <Tab.Screen name="WishList" component={WishList} />
+      <Tab.Screen name="MyCart" component={MyCart} />
+      <Tab.Screen name="accountspage" component={AccountpageRoutes} />
+    </Tab.Navigator>
   );
 }
 
-export default MainTabBar
+export default MainTabBar;
